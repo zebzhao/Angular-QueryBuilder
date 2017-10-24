b616c9bc9227a80046e84bb00f4f6344ef79734f
Zeb Zhao <zeb.zhao@global.local>
2017-10-24 14:34:55 -0400

    Updates

--- demo/main.js
+++ demo/main.js
@@ -203,7 +203,7 @@ var styles_AppComponent = ['.margin30[_ngcontent-%COMP%] { margin: 30px; }',
 exports.RenderType_AppComponent = i0.ɵcrt({ encapsulation: 0, styles: styles_AppComponent,
     data: {} });
 function View_AppComponent_0(_l) {
-    return i0.ɵvid(0, [(_l()(), i0.ɵeld(0, null, null, 1, 'vr-query-builder', [['class', 'margin30']], null, null, null, i1.View_QueryBuilderComponent_0, i1.RenderType_QueryBuilderComponent)), i0.ɵdid(638976, null, 0, i2.QueryBuilderComponent, [], { data: [0, 'data'], config: [1, 'config'] }, null), (_l()(), i0.ɵted(null, ['\n  '])), (_l()(), i0.ɵeld(0, null, null, 5, 'div', [['class', 'margin30']], null, null, null, null, null)), (_l()(),
+    return i0.ɵvid(0, [(_l()(), i0.ɵeld(0, null, null, 1, 'query-builder', [['class', 'margin30']], null, null, null, i1.View_QueryBuilderComponent_0, i1.RenderType_QueryBuilderComponent)), i0.ɵdid(638976, null, 0, i2.QueryBuilderComponent, [], { data: [0, 'data'], config: [1, 'config'] }, null), (_l()(), i0.ɵted(null, ['\n  '])), (_l()(), i0.ɵeld(0, null, null, 5, 'div', [['class', 'margin30']], null, null, null, null, null)), (_l()(),
             i0.ɵted(null, ['\n      '])), (_l()(), i0.ɵeld(0, null, null, 2, 'textarea', [], null, null, null, null, null)), (_l()(), i0.ɵted(null, ['', ''])), i0.ɵpid(0, i3.JsonPipe, []), (_l()(), i0.ɵted(null, ['\n  ']))], function (_ck, _v) {
         var _co = _v.component;
         var currVal_0 = _co.query;
@@ -704,7 +704,7 @@ function View_QueryBuilderComponent_4(_l) {
     });
 }
 function View_QueryBuilderComponent_15(_l) {
-    return i0.ɵvid(0, [(_l()(), i0.ɵeld(0, null, null, 1, 'vr-query-builder', [], null, null, null, View_QueryBuilderComponent_0, exports.RenderType_QueryBuilderComponent)), i0.ɵdid(638976, null, 0, i3.QueryBuilderComponent, [], { operatorMap: [0, 'operatorMap'], typeMap: [1, 'typeMap'], parentData: [2,
+    return i0.ɵvid(0, [(_l()(), i0.ɵeld(0, null, null, 1, 'query-builder', [], null, null, null, View_QueryBuilderComponent_0, exports.RenderType_QueryBuilderComponent)), i0.ɵdid(638976, null, 0, i3.QueryBuilderComponent, [], { operatorMap: [0, 'operatorMap'], typeMap: [1, 'typeMap'], parentData: [2,
                 'parentData'], data: [3, 'data'], config: [4, 'config'] }, null)], function (_ck, _v) {
         var _co = _v.component;
         var currVal_0 = _co.operatorMap;
@@ -911,12 +911,12 @@ function View_QueryBuilderComponent_0(_l) {
 }
 exports.View_QueryBuilderComponent_0 = View_QueryBuilderComponent_0;
 function View_QueryBuilderComponent_Host_0(_l) {
-    return i0.ɵvid(0, [(_l()(), i0.ɵeld(0, null, null, 1, 'vr-query-builder', [], null, null, null, View_QueryBuilderComponent_0, exports.RenderType_QueryBuilderComponent)), i0.ɵdid(638976, null, 0, i3.QueryBuilderComponent, [], null, null)], function (_ck, _v) {
+    return i0.ɵvid(0, [(_l()(), i0.ɵeld(0, null, null, 1, 'query-builder', [], null, null, null, View_QueryBuilderComponent_0, exports.RenderType_QueryBuilderComponent)), i0.ɵdid(638976, null, 0, i3.QueryBuilderComponent, [], null, null)], function (_ck, _v) {
         _ck(_v, 1, 0);
     }, null);
 }
 exports.View_QueryBuilderComponent_Host_0 = View_QueryBuilderComponent_Host_0;
-exports.QueryBuilderComponentNgFactory = i0.ɵccf('vr-query-builder', i3.QueryBuilderComponent, View_QueryBuilderComponent_Host_0, { operatorMap: 'operatorMap',
+exports.QueryBuilderComponentNgFactory = i0.ɵccf('query-builder', i3.QueryBuilderComponent, View_QueryBuilderComponent_Host_0, { operatorMap: 'operatorMap',
     typeMap: 'typeMap', parentData: 'parentData', data: 'data', config: 'config' }, {}, []);
 
 
@@ -984,7 +984,7 @@ var AppComponent = /** @class */ (function () {
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<vr-query-builder class='margin30' [data]='query' [config]='config'></vr-query-builder>\n  <div class='margin30'>\n      <textarea >{{query | json}}</textarea>\n  </div>",
+            template: "<query-builder class='margin30' [data]='query' [config]='config'></query-builder>\n  <div class='margin30'>\n      <textarea >{{query | json}}</textarea>\n  </div>",
             styles: ['.margin30 { margin: 30px; }', 'textarea { width: 100%; height: 250px; }']
         })
     ], AppComponent);
@@ -1006,19 +1006,21 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 var QueryBuilderComponent = /** @class */ (function () {
     function QueryBuilderComponent() {
         this.defaultEmptyList = [];
+        this.data = { condition: 'and', rules: [] };
+        this.config = { fields: {} };
         this.typeMap = {
-            string: "text",
-            number: "number",
-            category: "select",
-            date: "date",
-            boolean: "checkbox"
+            string: 'text',
+            number: 'number',
+            category: 'select',
+            date: 'date',
+            boolean: 'checkbox'
         };
         this.operatorMap = {
-            string: ["=", "!=", "contains", "like"],
-            number: ["=", "!=", ">", ">=", "<", "<="],
-            category: ["=", "!="],
-            date: ["=", "!=", ">", ">=", "<", "<="],
-            boolean: ["="]
+            string: ['=', '!=', 'contains', 'like'],
+            number: ['=', '!=', '>', '>=', '<', '<='],
+            category: ['=', '!='],
+            date: ['=', '!=', '>', '>=', '<', '<='],
+            boolean: ['=']
         };
     }
     QueryBuilderComponent.prototype.ngOnInit = function () {
@@ -1030,7 +1032,7 @@ var QueryBuilderComponent = /** @class */ (function () {
             this.operatorsCache = {};
         }
         else {
-            throw new Error("config must be a valid object");
+            throw new Error('config must be a valid object');
         }
     };
     QueryBuilderComponent.prototype.getOperators = function (field) {
@@ -1062,11 +1064,11 @@ var QueryBuilderComponent = /** @class */ (function () {
         }
         var type = this.config.fields[field].type;
         switch (operator) {
-            case "is null":
-            case "is not null":
+            case 'is null':
+            case 'is not null':
                 return null; // No displayed component
-            case "in":
-            case "not in":
+            case 'in':
+            case 'not in':
                 return 'multiselect';
             default:
                 return this.typeMap[type];
@@ -1106,7 +1108,7 @@ var QueryBuilderComponent = /** @class */ (function () {
             this.config.addRuleSet(parent);
         }
         else {
-            parent.rules = parent.rules.concat([{ condition: "and", rules: [] }]);
+            parent.rules = parent.rules.concat([{ condition: 'and', rules: [] }]);
         }
     };
     QueryBuilderComponent.prototype.removeRuleSet = function (ruleset, parent) {
@@ -1124,8 +1126,8 @@ var QueryBuilderComponent = /** @class */ (function () {
     };
     QueryBuilderComponent.decorators = [
         { type: __WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"], args: [{
-                    selector: 'vr-query-builder',
-                    template: "\n    <div class=\"switch-field float-right\">\n        <button (click)=\"addRule(data)\"><i class=\"close-icon\">\u2795</i> Rule</button>\n        <button (click)=\"addRuleSet(data)\"><i class=\"close-icon\">\u2795</i> Ruleset</button>\n        <ng-container *ngIf=\"!!parentData\">\n            <button (click)=\"removeRuleSet(data, parentData)\" class=\"danger\"><i class=\"close-icon\">\u274C</i></button>\n        </ng-container>\n    </div>\n\n    <div class=\"switch-field\">\n        <input type=\"radio\" [(ngModel)]=\"data.condition\" value=\"and\" #andOption/>\n        <label (click)=\"data.condition=andOption.value\">AND</label>\n        <input type=\"radio\" [(ngModel)]=\"data.condition\" value=\"or\" #orOption/>\n        <label (click)=\"data.condition=orOption.value\">OR</label>\n    </div>\n\n    <ul class=\"query-tree\">\n      <ng-container *ngFor=\"let item of data.rules\">\n        <ng-container *ngIf=\"{ruleset: !!item.rules, invalid: !config.allowEmptyRuleset && item.rules && item.rules.length === 0} as local\">\n          <li [ngClass]=\"{'query-item': true, 'rule': !local.ruleset, 'ruleset': local.ruleset, 'invalid-ruleset': local.invalid}\">\n            <ng-container *ngIf=\"!local.ruleset\">\n                <div class=\"switch-field float-right\">\n                  <button class=\"danger\" (click)=\"removeRule(item, data)\"><i class=\"close-icon\">\u274C</i></button>\n                </div>\n\n                <select class=\"form-control\" [(ngModel)]=\"item.field\" (change)=\"onFieldChange(item)\">\n                  <option *ngFor=\"let field of fieldNames\" [ngValue]=\"field\">\n                    {{config.fields[field].name}}\n                  </option>\n                </select>\n                <select class=\"form-control\" [(ngModel)]=\"item.operator\">\n                  <option *ngFor=\"let operator of getOperators(item.field)\" [ngValue]=\"operator\">\n                    {{operator}}\n                  </option>\n                </select>\n                <ng-container [ngSwitch]=\"getInputType(item.field, item.operator)\">\n                  <input class=\"form-control\" [(ngModel)]=\"item.value\" *ngSwitchCase=\"'text'\" type=\"text\">\n                  <input class=\"form-control\" [(ngModel)]=\"item.value\" *ngSwitchCase=\"'date'\" type=\"date\">\n                  <input class=\"form-control\" [(ngModel)]=\"item.value\" *ngSwitchCase=\"'number'\" type=\"number\">\n                  <select class=\"form-control\" [(ngModel)]=\"item.value\" *ngSwitchCase=\"'select'\">\n                    <option *ngFor=\"let opt of getOptions(item.field)\" [ngValue]=\"opt.value\">\n                      {{opt.name}}\n                    </option>\n                  </select>\n                  <ng-container *ngSwitchCase=\"'multiselect'\">\n                    <div style=\"margin-bottom: 8px\"></div>\n                    <select class=\"form-control\" [(ngModel)]=\"item.value\" style=\"min-width: 200px\" multiple>\n                      <option *ngFor=\"let opt of getOptions(item.field)\" [ngValue]=\"opt.value\">\n                        {{opt.name}}\n                      </option>\n                    </select>\n                  </ng-container>\n                  <input class=\"form-control\" [(ngModel)]=\"item.value\" *ngSwitchCase=\"'checkbox'\" type=\"checkbox\">\n                </ng-container>\n            </ng-container>\n            <vr-query-builder *ngIf=\"local.ruleset\" [data]=\"item\" [parentData]=\"data\" [config]=\"config\" [typeMap]=\"typeMap\" [operatorMap]=\"operatorMap\"></vr-query-builder>\n            <p class=\"empty-warning\" *ngIf=\"local.invalid\">A ruleset cannot be empty. Please add a rule or remove it all together.</p>        \n          </li>\n        </ng-container>\n      </ng-container>\n    </ul>\n  ",
+                    selector: 'query-builder',
+                    template: "\n    <div class=\"switch-field float-right\">\n        <button (click)=\"addRule(data)\"><i class=\"close-icon\">\u2795</i> Rule</button>\n        <button (click)=\"addRuleSet(data)\"><i class=\"close-icon\">\u2795</i> Ruleset</button>\n        <ng-container *ngIf=\"!!parentData\">\n            <button (click)=\"removeRuleSet(data, parentData)\" class=\"danger\"><i class=\"close-icon\">\u274C</i></button>\n        </ng-container>\n    </div>\n\n    <div class=\"switch-field\">\n        <input type=\"radio\" [(ngModel)]=\"data.condition\" value=\"and\" #andOption/>\n        <label (click)=\"data.condition=andOption.value\">AND</label>\n        <input type=\"radio\" [(ngModel)]=\"data.condition\" value=\"or\" #orOption/>\n        <label (click)=\"data.condition=orOption.value\">OR</label>\n    </div>\n\n    <ul class=\"query-tree\">\n      <ng-container *ngFor=\"let item of data.rules\">\n        <ng-container *ngIf=\"{ruleset: !!item.rules, invalid: !config.allowEmptyRuleset && item.rules && item.rules.length === 0} as local\">\n          <li [ngClass]=\"{'query-item': true, 'rule': !local.ruleset, 'ruleset': local.ruleset, 'invalid-ruleset': local.invalid}\">\n            <ng-container *ngIf=\"!local.ruleset\">\n                <div class=\"switch-field float-right\">\n                  <button class=\"danger\" (click)=\"removeRule(item, data)\"><i class=\"close-icon\">\u274C</i></button>\n                </div>\n\n                <select class=\"form-control\" [(ngModel)]=\"item.field\" (change)=\"onFieldChange(item)\">\n                  <option *ngFor=\"let field of fieldNames\" [ngValue]=\"field\">\n                    {{config.fields[field].name}}\n                  </option>\n                </select>\n                <select class=\"form-control\" [(ngModel)]=\"item.operator\">\n                  <option *ngFor=\"let operator of getOperators(item.field)\" [ngValue]=\"operator\">\n                    {{operator}}\n                  </option>\n                </select>\n                <ng-container [ngSwitch]=\"getInputType(item.field, item.operator)\">\n                  <input class=\"form-control\" [(ngModel)]=\"item.value\" *ngSwitchCase=\"'text'\" type=\"text\">\n                  <input class=\"form-control\" [(ngModel)]=\"item.value\" *ngSwitchCase=\"'date'\" type=\"date\">\n                  <input class=\"form-control\" [(ngModel)]=\"item.value\" *ngSwitchCase=\"'number'\" type=\"number\">\n                  <select class=\"form-control\" [(ngModel)]=\"item.value\" *ngSwitchCase=\"'select'\">\n                    <option *ngFor=\"let opt of getOptions(item.field)\" [ngValue]=\"opt.value\">\n                      {{opt.name}}\n                    </option>\n                  </select>\n                  <ng-container *ngSwitchCase=\"'multiselect'\">\n                    <div style=\"margin-bottom: 8px\"></div>\n                    <select class=\"form-control\" [(ngModel)]=\"item.value\" style=\"min-width: 200px\" multiple>\n                      <option *ngFor=\"let opt of getOptions(item.field)\" [ngValue]=\"opt.value\">\n                        {{opt.name}}\n                      </option>\n                    </select>\n                  </ng-container>\n                  <input class=\"form-control\" [(ngModel)]=\"item.value\" *ngSwitchCase=\"'checkbox'\" type=\"checkbox\">\n                </ng-container>\n            </ng-container>\n            <query-builder *ngIf=\"local.ruleset\" [data]=\"item\" [parentData]=\"data\" [config]=\"config\" [typeMap]=\"typeMap\" [operatorMap]=\"operatorMap\"></query-builder>\n            <p class=\"empty-warning\" *ngIf=\"local.invalid\">A ruleset cannot be empty. Please add a rule or remove it all together.</p>        \n          </li>\n        </ng-container>\n      </ng-container>\n    </ul>\n  ",
                     styles: ["\n    :host{display:block}:host .close-icon{font-style:normal;font-size:12px}:host .switch-field{font-family:\"Lucida Grande\", Tahoma, Verdana, sans-serif;overflow:hidden}:host .float-right{float:right}:host .switch-title{margin-bottom:6px}:host .form-control{display:inline-block;padding:4px 8px;color:#555;background-color:#fff;background-image:none;border:1px solid #ccc;border-radius:4px}:host .switch-field input{position:absolute;clip:rect(0, 0, 0, 0);height:1px;width:1px;border:0;overflow:hidden}:host .switch-field button{margin-left:8px;background-color:white}:host .switch-field label{background-color:#e4e4e4}:host .switch-field label,:host .switch-field button{float:left;min-width:30px;color:rgba(0,0,0,0.6);font-size:14px;font-weight:normal;text-align:center;text-shadow:none;padding:4px 8px;border:1px solid rgba(0,0,0,0.2);-webkit-transition:all 0.1s ease-in-out;-moz-transition:all 0.1s ease-in-out;-ms-transition:all 0.1s ease-in-out;-o-transition:all 0.1s ease-in-out;transition:all 0.1s ease-in-out}:host .switch-field label:hover,:host .switch-field button:hover{cursor:pointer;background-color:#F0F0F0}:host .switch-field label.success,:host .switch-field button.success{color:#75BE47}:host .switch-field label.danger,:host .switch-field button.danger{color:#B3415D}:host .switch-field input:checked+label{border:1px solid #619ed7;background:white;color:#3176b3}:host .invalid-ruleset{border:1px solid rgba(179,65,93,0.5) !important;background:rgba(179,65,93,0.1) !important}:host .empty-warning{color:#8d252e;text-align:center}:host .query-tree{list-style:none;margin:4px 0 2px}:host .query-tree .query-item{position:relative;padding:4px 6px;margin-top:4px;border:1px solid #CCCCCC;-webkit-transition:all 0.1s ease-in-out;-moz-transition:all 0.1s ease-in-out;-ms-transition:all 0.1s ease-in-out;-o-transition:all 0.1s ease-in-out;transition:all 0.1s ease-in-out}:host .query-tree .query-item.ruleset{background:rgba(204,204,204,0.2)}:host .query-tree .query-item.ruleset:hover{border:1px solid rgba(97,158,215,0.5);background:rgba(97,158,215,0.1)}:host .query-tree .query-item.rule{background:white}:host .query-tree .query-item::before{top:-5px;border-width:0 0 2px 2px}:host .query-tree .query-item::after{border-width:0 0 0 2px;top:50%}:host .query-tree .query-item::before,:host .query-tree .query-item::after{content:'';left:-12px;border-color:#CCC;border-style:solid;width:9px;height:calc(50% + 6px);position:absolute}:host .query-tree .query-item:last-child::after{content:none}\n  "]
                 },] },
     ];
